f8c0a0b5d07d893221ca430940315d2faceca97c
ssrb (via Travis CI) <sb@unhexhexium.net>
2016-02-22 14:22:56 +1300

    Menger sponge works

--- structure-artist.ts
+++ structure-artist.ts
@@ -26,13 +26,49 @@
 // either expressed or implied, of the FreeBSD Project.
 
 ///<reference path="typings/browserify/browserify.d.ts"/>
-
+///<reference path="typings/gl-matrix/gl-matrix.d.ts"/>
+var glmat = require('./bower_components/gl-matrix/dist/gl-matrix-min.js');
 import ShapeInstance = require('./structure');
 
 class StructureArtist {
+
 	public constructor(gl: WebGLRenderingContext, structure: ShapeInstance[]) {
 		this.gl = gl;
+		this.structure = structure;
 		this.init();
+
+		var triangles = [0, 1, 2, 1, 2, 3,
+			4, 5, 6, 5, 6, 7,
+			0, 1, 4, 1, 4, 5,
+			2, 3, 6, 3, 6, 7,
+			0, 2, 4, 2, 4, 6,
+			1, 3, 5, 3, 5, 7];
+
+        var vertices = [0, 0, 0,
+        				0, 0, 1,
+        				0, 1, 0,
+        				0, 1, 1,
+        				1, 0, 0,
+        				1, 0, 1,
+        				1, 1, 0,
+        				1, 1, 1];
+
+
+		this.allvertices = [];
+		this.alltriangles = [];
+
+		// TODO: do all that in the web worker and on the GPU !
+		for (var si = 0; si < structure.length; ++si) {
+			for (var vi = 0; vi < 8; ++vi) {
+				var vert = [0, 0, 0, 0];
+				glmat.vec4.transformMat4(vert, [vertices[3 * vi], vertices[3 * vi + 1], vertices[3 * vi + 2], 1], structure[si].geospace);
+				this.allvertices.push(vert[0], vert[1], vert[2]);
+			}
+			var tris = [];
+			for (var fi = 0; fi < 12 * 3; ++fi) {
+				this.alltriangles.push(triangles[fi] + si * 8);
+			}
+		}
 	}
 
 	private init() {
@@ -54,29 +90,12 @@ class StructureArtist {
 
 	public draw(prMatrix: Float32Array, mvMatrix: Float32Array): void {
 		
-		var vertices = [0, 0, 0,
-        				0, 0, 1,
-        				0, 1, 0,
-        				0, 1, 1,
-        				1, 0, 0,
-        				1, 0, 1,
-        				1, 1, 0,
-        				1, 1, 1];
-
-        var triangles = [	0, 1, 2, 1, 2, 3,
-							4, 5, 6, 5, 6, 7,
-							0, 1, 4, 1, 4, 5,
-							2, 3, 6, 3, 6, 7,
-							0, 2, 4, 2, 4, 6,
-							1, 3, 5, 3, 5, 7];
-     
 		var gl = this.gl;
-       
-        gl.useProgram(this.structureProgram);
+		gl.useProgram(this.structureProgram);
 
         var location = gl.getAttribLocation(this.structureProgram, 'aPos')
         gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.allvertices), gl.STATIC_DRAW);
         gl.vertexAttribPointer(location, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(location);
 
@@ -87,12 +106,13 @@ class StructureArtist {
         // gl.enableVertexAttribArray(solLocation);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(triangles), gl.STATIC_DRAW);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.alltriangles), gl.STATIC_DRAW);
 
         gl.uniformMatrix4fv(gl.getUniformLocation(this.structureProgram, 'prMatrix'), false, prMatrix);
-        gl.uniformMatrix4fv(gl.getUniformLocation(this.structureProgram, 'mvMatrix'), false, mvMatrix);
+		gl.uniformMatrix4fv(gl.getUniformLocation(this.structureProgram, 'mvMatrix'), false, mvMatrix);
        
-        gl.drawElements(gl.TRIANGLES, triangles.length, gl.UNSIGNED_SHORT, 0);      
+        gl.drawElements(gl.TRIANGLES, this.alltriangles.length, gl.UNSIGNED_SHORT, 0);
+
 	}
 
 	private static getShader(gl : WebGLRenderingContext, path : string) : WebGLShader {
@@ -119,5 +139,9 @@ class StructureArtist {
 
 	private gl: WebGLRenderingContext;
  	private structureProgram: WebGLProgram;
+  	private structure: ShapeInstance[];
+	private allvertices: number[];
+	private alltriangles: number[];
+
 }
 export = StructureArtist;
